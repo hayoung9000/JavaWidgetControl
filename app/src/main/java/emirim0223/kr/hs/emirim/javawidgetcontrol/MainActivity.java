d88c344d9cd0ba6a8fb505b8c3d01fee6f78a1ab
hayoung9000 <wjsgkdud9000@naver.com>
2017-03-23 20:08:48 +0900

    #2 change widget in the java

--- app/src/main/java/emirim0223/kr/hs/emirim/javawidgetcontrol/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/javawidgetcontrol/MainActivity.java
@@ -1,13 +1,38 @@
 package emirim0223.kr.hs.emirim.javawidgetcontrol;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //이게 없으면 findViewfile을 못한다
+
+        TextView text1=(TextView)findViewById(R.id.text1);
+        TextView text2=(TextView)findViewById(R.id.text2);
+        TextView text3=(TextView)findViewById(R.id.text3);
+        TextView text4=(TextView)findViewById(R.id.text4);
+
+        text1.setText("First TextView");
+        text1.setTextSize(30);
+        text1.setBackgroundColor(Color.rgb(255,0,255));
+        text1.setTextColor(Color.WHITE);
+
+        text2.setText(R.string.text2_str);
+        text2.setTypeface(Typeface.SANS_SERIF,Typeface.BOLD_ITALIC);
+
+        text3.setTextSize(20);
+        text3.setText("Vitamin H");
+        text3.setBackgroundColor(Color.BLACK);
+        text3.setTextColor(Color.WHITE);
+
+        text4.setText("Fourth TextView");
+        text4.setTextSize(50);
+        text4.setBackgroundColor(Color.argb(170,255,0,0));
     }
 }
